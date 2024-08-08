9092ed1f24e0e36322661eed5e0a68b50d5ac5f1
Andrey Torkhov <torkhovasic@yandex.ru>
2024-08-08 16:05:52 +0400

    add PersonPage/ add UiLoader

--- src/routes/routesConfig.js
+++ src/routes/routesConfig.js
@@ -1,5 +1,6 @@
 import HomePage from "@containers/HomePage";
 import PeoplePage from "@containers/PeoplePage";
+import PersonPage from "@containers/PersonPage/PersonPage";
 import NotFoundPage from "@containers/NotFoundPage";
 
 const routesConfig = [
@@ -10,6 +11,10 @@ const routesConfig = [
   {
     path: "/people",
     component: PeoplePage,
+  },
+  {
+    path: "/people/:id",
+    component: PersonPage,
   },
   {
     path: "/not-found",
